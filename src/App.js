81f21bcca32e038fc70080b205f0ab2d609d2d7f
Slinkshot Srl <info@slinkshot.tech>
2022-08-19 19:05:31 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const src =
+"https://storageapi.fleek.co/8b69b791-a113-4a7f-8d37-f4905b484016-bucket/panasonic-hokkaido-and-tokyo-uhd-(www.demolandia.net).mp4";
+
+class App extends Component {
+
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +24,14 @@ function App() {
         >
           Learn React
         </a>
+        <video controls width="100%">
+      <source src={src} type="video/mp4" />
+      Sorry, your browser doesn't support embedded videos.
+    </video>
       </header>
     </div>
   );
+}
 }
 
 export default App;
